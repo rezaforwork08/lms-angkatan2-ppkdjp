8eb93790a0ae28407ac7fa7d59c4adc4f2bc2a11
rezaforwork08 <rezaforwork08@gmail.com>
2025-06-11 15:11:49 +0700

    Restore

--- content/restore-user.php
+++ content/restore-user.php
@@ -1,7 +1,23 @@
 <?php
-$query = mysqli_query($config, "SELECT * FROM users WHERE deleted_at = 0 ORDER BY id DESC");
+$query = mysqli_query($config, "SELECT * FROM users WHERE deleted_at = 1 ORDER BY id DESC");
 // 12345, 54321
 $rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+if (isset($_GET['restore'])) {
+    $idRestore = $_GET['restore'];
+    $queryRestore = mysqli_query($config, "UPDATE users SET deleted_at = 0 WHERE id = $idRestore");
+    if ($queryRestore) {
+        header("location:?page=user");
+    }
+}
+if (isset($_GET['delete'])) {
+    $idDel = $_GET['delete'];
+
+    $qDelete = mysqli_query($config, "DELETE FROM users WHERE id = $idDel");
+    if ($qDelete) {
+        header("location:?page=user ");
+    }
+}
 ?>
 <div class="row">
     <div class="col-12">
@@ -9,8 +25,7 @@ $rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
             <div class="card-body">
                 <h5 class="card-title">Data User</h5>
                 <div class="mb-3 d-flex justify-content-between">
-                    <a href="?page=tambah-user" class="btn btn-primary">Add User</a>
-                    <a href="?page=restore-user" class="btn btn-primary">Restore</a>
+                    <a href="?page=user" class="btn btn-secondary">Back</a>
                 </div>
                 <div class="table-responsive">
                     <table class="table table-bordered">
@@ -29,10 +44,10 @@ $rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
                                     <td><?php echo $row['name'] ?></td>
                                     <td><?php echo $row['email'] ?></td>
                                     <td>
-                                        <a href="?page=tambah-user&edit=<?php echo $row['id'] ?>"
-                                            class="btn btn-primary">Edit</a>
+                                        <a href="?page=restore-user&restore=<?php echo $row['id'] ?>"
+                                            class="btn btn-primary">Restore</a>
                                         <a onclick="return confirm('Are you sure wanna delete this data??')"
-                                            href="?page=tambah-user&delete=<?php echo $row['id'] ?>"
+                                            href="?page=restore-user&delete=<?php echo $row['id'] ?>"
                                             class="btn btn-danger">Delete</a>
 
                                     </td>
